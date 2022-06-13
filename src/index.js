bcd91bb752f3557ec14bdc6a8d51b397b6210dfd
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2022-06-13 19:38:36 +0200

    finished setting up pakages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.scss';
+
+function App() {
+  return (
+    <div>Home Page</div>
+  );
+}
+
+const root = createRoot(document.getElementById('main'));
+root.render(<App />);
